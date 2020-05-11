0088413914bf8732b6a0b3b81da131cca40efcdb
WRBH <hxy610939654@live.com>
2020-05-11 17:11:42 +0800

    classify

---
+++ ajax/ajax.js
@@ -0,0 +1,12 @@
+/*
+* @Author: WRBH
+* @Date:   2020-05-11 16:40:20
+* @Last Modified by:   WRBH
+* @Last Modified time: 2020-05-11 17:08:47
+*/
+var $ = {
+    ajax:function(options){
+        var xhr = null;
+        
+    }
+}
